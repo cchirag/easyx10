da765c5981e3abeaf42eb8ee446c53ed23113a1e
jjbagoose <jjbagoose@299a14cc-0856-0410-862c-1366464cdd17>
2008-09-05 02:59:22 +0000

    Adding New Test Java project to test subversive plugin

---
+++ cs673_test_john/src/edu/bu/cs673/TestMain.java
@@ -0,0 +1,16 @@
+package edu.bu.cs673;
+
+/*
+ * Simple test class 
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is a test");
+
+	}
+
+}
