94420b4cda4fbb887bc95f4bc371ef9c28a2d468
jim@duda.tzo.com <jim@duda.tzo.com@299a14cc-0856-0410-862c-1366464cdd17>
2008-11-09 21:21:41 +0000

    I provided a default portName as a function of linux or windows.

--- trunk/EasyX10_Application/src/edu/bu/easyx10/protocol/ProtocolFactory.java
+++ trunk/EasyX10_Application/src/edu/bu/easyx10/protocol/ProtocolFactory.java
@@ -25,20 +25,48 @@ public class ProtocolFactory {
 
 	/**
 	 * The initProtocol method is responsible for instantiating all the 
-	 * available protocols.
+	 * available protocols.  We only want to do this once, so we guard
+	 * it with synchronized and a boolean control flag.
 	 */
 	public synchronized static void initProtocols( ) throws Exception {
+		
+		// create some local method variables    
+		String osType = "";
+		
+        // Determine our operating system environment			
+		try {
+	      osType= System.getProperty("os.arch");
+	      System.out.println("Operating system type =>" + osType);
+	    } catch (Exception e) {
+	      System.out.println("Exception caught =" + e.getMessage());
+	    }
+
 		// instantiate all the possible protocols here
 		// @TODO - the serial port needs to come from the configuration package
-		if (protocolInitialized == false) {
-			try {
-			    x10Protocol = new CM11A_X10Protocol( "/dev/ttyUSB0" );
+ 		if (protocolInitialized == false) {
+
+ 			// Initialize the X10 Protocol Driver
+ 			String x10Port;
+
+ 			// Determine the default portName
+ 			if (osType.equals("linux")) {
+ 			    x10Port = "/dev/ttyUSB0";
+ 			} else if (osType.equals("amd64")) {
+ 			    x10Port = "/dev/ttyUSB0";
+ 			} else {
+ 			    x10Port = "COM4";
+ 			}
+
+ 		    // Now instantiate the x10Protocol using our default port name.
+ 		    try {
+			    x10Protocol = new CM11A_X10Protocol( x10Port );
 			} catch ( Exception e ) {
 				System.out.println ( "Error attempting to initialize CM11A_X10Protocol\n");
 				System.out.println ( e );
 				throw e;
 			};
-			// mark the protocol factory as initialized
+			
+			// finish by marking the protocol factory as initialized
 			protocolInitialized = true;
 		}
 	}
