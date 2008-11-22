06e9617738dfb09c705d0fbae5c257af63db6af2
damon.gabrielle <damon.gabrielle@gmail.com>
2008-11-22 04:30:24 +0000

    Completed TriggerTimer

--- EasyX10_Application/src/edu/bu/easyx10/device/timer/TriggerTimer.java
+++ EasyX10_Application/src/edu/bu/easyx10/device/timer/TriggerTimer.java
@@ -1,78 +1,93 @@
 package edu.bu.easyx10.device.timer;
 
 import edu.bu.easyx10.event.Event;
-import java.sql.Time;  //TODO remove ASAP
+import edu.bu.easyx10.util.LoggingUtilities;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Timer;
-//import java.text.SimpleDateFormat.*;
+
 
 
 
 public class TriggerTimer extends DeviceTimer{
 
 	// private member variables
-	private Time mTriggerTime;
+	   
+	private Calendar mTriggerTime = Calendar.getInstance();
 	private Timer timer = new Timer();
+	String DATE_FORMAT_NOW = "H:mm:ss:SSS";
+	SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
 
-
-	
-	public TriggerTimer(Event eventToFire, Time triggerTime){
-		super(eventToFire);
-
-	}
 	
 	public TriggerTimer(Event eventToFire, Calendar triggerTime){
 		super(eventToFire);
-		//setTriggerTime(triggerTime);
-		
+		setTriggerTime(triggerTime);	
 	}
 	
 	
-	public Time getTriggerTime() {
+	public Calendar getTriggerTime() {
 		return mTriggerTime;
 	}
 
-	public void setTriggerTime(Time triggerTime) {
-		mTriggerTime = triggerTime;
+	public void setTriggerTime(Calendar triggerTime) {
+		
+		/* 
+		 * We don't really restrict which Calendar fields are set on triggerTime
+		 * when it's passed in. However, we only care about time of day
+		 * so we should only set those values in mTriggerTime.
+		 */
+		
+		mTriggerTime.set(Calendar.HOUR_OF_DAY, triggerTime.get(Calendar.HOUR_OF_DAY));
+		mTriggerTime.set(Calendar.MINUTE, triggerTime.get(Calendar.MINUTE));
+		mTriggerTime.set(Calendar.SECOND, triggerTime.get(Calendar.SECOND));
+		mTriggerTime.set(Calendar.MILLISECOND, triggerTime.get(Calendar.MILLISECOND));
+		
 	}
 	
-	//public void setTriggerTime(Calendar triggerTime) {
-		//mTriggerDate = triggerTime;
-	//}
 
 	@Override
 	public void run() {
 
-		Calendar currentTime = Calendar.getInstance();
-		String DATE_FORMAT_NOW = "H:mm:ss:SSS";
-		SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
-        System.out.println("The current time is " + sdf);
-		System.out.println("I did something at " + sdf.format(currentTime.getTime()));
+		eventGenerator.fireEvent(mEventToFire);
 		
-		//eventGenerator.fireEvent(mEventToFire);
+		//Log a message indicating the timer was started
+		LoggingUtilities.logInfo(TriggerTimer.class.getCanonicalName(),
+				 "run()","INFO: Trigger Timer Fired and " + mEventToFire.toString() + 
+				 " event to " + mEventToFire.getDeviceName());
 		
 	}
 
 	@Override
 	public Event getEventToFire() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		
+		return mEventToFire;
 	}
 
 	@Override
 	public void setEventToFire(Event eventToFire) {
-		// TODO Auto-generated method stub
+
+		mEventToFire = eventToFire;
 		
 	}
 
 	@Override
 	public void startTimer() {
 
-		//Schedule the execution of run(),at mTriggerTime, every 24 hrs
-		//timer.scheduleAtFixedRate(this, mTriggerDate.getTime(), 86400000);
-		timer.scheduleAtFixedRate(this, 0, 10000);
+		//Useful Debugging
+			//System.out.println("I will do something at " + sdf.format(mTriggerTime.getTime()));
 		
+		//clean up previous scheduled tasks
+		timer.cancel();  //cancel the previous scheduled task because the time may have changed
+		timer.purge();   //Tell garbage collection to mark canceled tasks for cleanup
+		
+		//Schedule the TriggerTimer to fire once every 24 hrs from mTriggerTime
+		timer.schedule(this,mTriggerTime.getTime(), 86400000);
+		
+		//Log a message indicating the timer was started
+		LoggingUtilities.logInfo(TriggerTimer.class.getCanonicalName(),
+				 "startTimer()","INFO: TriggerTimer was started. An event will " +
+				 "fire at " + sdf.format(mTriggerTime.getTime()));
 	}
 
 }
