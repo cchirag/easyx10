2a2f1970da9affd7b6932b1d0419bf114029a5d3
jjbagoose <jjbagoose@gmail.com>
2008-11-23 00:06:47 +0000

    Adding Javascript files and new AddMotionSensor Page

---
+++ EasyX10_Application/web/javascripts/_vti_cnf/controls.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|04 Jan 2008 02:07:14 -0000
+vti_extenderversion:SR|12.0.0.4518
+vti_author:SR|Windy-PC\\John
+vti_modifiedby:SR|Windy-PC\\John
+vti_timecreated:TR|04 Jan 2008 02:07:14 -0000
+vti_cacheddtm:TX|04 Jan 2008 02:07:14 -0000
+vti_filesize:IR|34868
+vti_backlinkinfo:VX|
