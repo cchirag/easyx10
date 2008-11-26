8976c0fbe63e72cebe7699d6563eef0d6046d955
jim@duda.tzo.com <jim@duda.tzo.com@299a14cc-0856-0410-862c-1366464cdd17>
2008-11-26 03:01:43 +0000

    Full implementation of a WaitTimer which uses a single Thread and sleep.

--- trunk/EasyX10_Application/src/edu/bu/easyx10/device/timer/WaitTimer.java
+++ trunk/EasyX10_Application/src/edu/bu/easyx10/device/timer/WaitTimer.java
@@ -1,57 +1,109 @@
 package edu.bu.easyx10.device.timer;
 
 import edu.bu.easyx10.event.*;
+import edu.bu.easyx10.util.LoggingUtilities;
 
-public class WaitTimer extends DeviceTimer{
+/**
+ * This is the concrete class for the WaitTimer class.  This version
+ * of the DeviceTimer provides a very simple function.  The object
+ * is given a number of seconds to wait.  Once the number of seconds
+ * has occurred, the Event defined by eventToFire is sent to the
+ * EventGenerator.
+ *
+ * @author:  Jim Duda
+ * @version: please refer to subversion
+ * @date:    10/30/08
+ *
+ */
+public class WaitTimer extends DeviceTimer {
 
 	// private member variables
 	int mSecondsToWait;
-	
+	int mCountDown;
+
+	/**
+	 * Construct a new WaitTimer object.  This object is given a 
+	 * number of seconds to wait and an EventToFire when the number
+	 * of seconds has occurred.
+	 * 
+	 * @param int   secondsToWait
+	 * @param Event eventToFire
+	 */
 	public WaitTimer(int secondsToWait, Event eventToFire){
+		// Create the base class
 		super(eventToFire);
+		// Store our member variables
 		setSecondsToWait(secondsToWait);
-		
-		startTimer();
+		// Start the timer on creation if there is a time to wait
+		mCountDown = -1;
 	}
-		
+	
+	/**
+	 * This access methods is used to load the seconds to wait.
+	 * 
+	 * @param int secondsToWait
+	 */
+	public void setSecondsToWait(int secondsToWait) {
+		//set the number of seconds to wait
+		mSecondsToWait = secondsToWait;
+	}
+
+	/**
+	 * This method is used to return the seconds to wait.
+	 * 
+	 * @return int
+	 */
 	public int getSecondsToWait() {
 		return mSecondsToWait;
 	}
 
+	/**
+	 * The run method provides the main timer function.  The run
+	 * method runs once per second.  Each second, the mCountDown
+	 * variable is checked for zero.  If zero, we send our event
+	 * along.  If non zero, we decrement.  We only decrement to
+	 * zero then stop at -1.
+	 * 
+	 */
+	public void run( ) {
 
-	public void setSecondsToWait(int secondsToWait) {
-		
-		//set the number of seconds to wait
-		mSecondsToWait = secondsToWait;
-		
-		//Restart the Timer
-		startTimer();
-		
+		while (true) {
+			// When the timer expires, send our Event to notify
+			if (mCountDown == 0) {
+				eventGenerator.fireEvent( getEventToFire( ) );
+			} 
+			// Decrement only until we reach -1
+			if (mCountDown > -1) {
+				mCountDown--;
+				LoggingUtilities.logInfo(this.getClass( ).getCanonicalName(), "run",
+						"mCountDown:: " + Integer.toString(mCountDown ) );
+			}
+			// Sleep for one second
+			try {
+				Thread.sleep(1000);
+			} catch ( InterruptedException e ) {
+				break;
+			};
+		}
 	}
 
-	@Override
-	public Event getEventToFire() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setEventToFire(Event eventToFire) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
+	/**
+	 * This method simply reloads the mCountDown variable to
+	 * allow the timer to operate.
+	 */
 	public void startTimer() {
-		// TODO Auto-generated method stub
-		
+		mCountDown = mSecondsToWait;
+		LoggingUtilities.logInfo(this.getClass( ).getCanonicalName(), "startTimer",
+				"mCountDown:: " + Integer.toString(mCountDown ) );
+	}
+
+
+	/**
+	 * This method simply forces the counter to the idle
+	 * condition of -1 to stop the timer from operating.
+	 */
+	public void stopTimer() {
+		mCountDown = -1;
 	}
 
 }
