de651b2e941ac77cba3ed8d14ba220f2a81204ae
damon.gabrielle <damon.gabrielle@gmail.com>
2008-11-30 05:40:00 +0000

    Added a descriptive comment in the header.

--- EasyX10_Application/src/edu/bu/easyx10/device/timer/DeviceTimer.java
+++ EasyX10_Application/src/edu/bu/easyx10/device/timer/DeviceTimer.java
@@ -5,6 +5,19 @@ import edu.bu.easyx10.event.Event;
 import edu.bu.easyx10.event.EventGenerator;
 import edu.bu.easyx10.event.EventGeneratorFactory;
 
+/**
+ * The DeviceTimer class is base class of the Timer package. It is an abstract 
+ * class used by the device class to perform time related events. It is
+ * only responsibility is to firing events to the EventGenerator at a specific
+ * moment in time. Actual device Timer implementation is handled by the derived
+ * classes. Timer contains only one attribute; Event EventToFire and contains 
+ * two methods to get and set EventToFire. It also contains an abstract method
+ * called startTimer() that must be overridden by the derived class.
+ * .
+ * @author dgabriel
+ * @version please refer to subversion
+ * @date:   11/29/08
+ */
 
 public abstract class DeviceTimer extends TimerTask {
 	
